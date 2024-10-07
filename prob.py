c13d70a956c2f99c59b2fb8c15d090f406747466
MrAbiotic <harald.helland.velde@gmail.com>
2024-10-07 22:48:42 +0200

    Numerical solution to probability

---
+++ prob.py
@@ -0,0 +1,21 @@
+import random
+
+# Using the fact that the numbers are either different or not
+# to add either 1 or 0 the sum, then dividing by the count
+
+n = int(1e7)
+s = 0
+
+for i in range(n):
+    if len({random.randint(0, 9),
+         random.randint(0, 9),
+         random.randint(0, 9),
+         random.randint(0, 9),
+         random.randint(0, 9),
+         random.randint(0, 9),
+         random.randint(0, 9),
+         random.randint(0, 9)}) == 8:
+        s += 1
+
+prob = s/n*100
+print(f"Probability: {prob:.5f} %")
